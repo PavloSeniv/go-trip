327689babe4b0ed8d09bc40d8a37735063a1927b
PavloSeniv <senivpavlo016@gmail.com>
2021-12-27 21:11:27 +0200

    Re-build project for gulp-build-2022

--- #src/js/_parallax_init.js
+++
@@ -1,5 +0,0 @@
-var mainSceneAuto = document.getElementById("man-scene");
-var parallax = new Parallax(mainSceneAuto, {
-  hoverOnly: true,
-  relativeInput: true,
-});
--- #src/js/main_script.js
+++
@@ -1,9 +0,0 @@
-@@include('_webp_add_class_to_html.js')
-
-@@include('_header_menu_burger.js')
-
-@@include('_parallax_init.js')
-
-// $(function(){
-
-// });
